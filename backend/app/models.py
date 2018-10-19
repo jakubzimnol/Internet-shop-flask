24b1b1c2376b798a2bf91c1f20ec32ee3c408cba
jakubzimnol <jakubzimnol@gmail.com>
2018-10-19 11:32:46 +0200

    fix models, add test for models SQLAlchemy, add db update to docker-compose file

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,6 @@
 import enum
 
-from app import db
+from init_app import db
 
 
 class Role(enum.Enum):
@@ -12,10 +12,12 @@ class Role(enum.Enum):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
     password_hash = db.Column(db.String(128))
-    bought = db.relationship('Item', backref='buyer', lazy=True)
-    sold = db.relationship('Item', backref='seller', lazy=True)
+    sold_item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
+    bought_item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
+    sold_item = db.relationship('Item', backref='seller', lazy=True, foreign_keys=[sold_item_id])
+    bought_item = db.relationship('Item', backref='buyer', lazy=True, foreign_keys=[bought_item_id])
     role = db.Column(db.Enum(Role))
 
     def __repr__(self):
@@ -25,11 +27,9 @@ class User(db.Model):
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    category = db.relationship('Category', backref='item', lazy=True, nullable=True)
-    subcategory = db.relationship('Subcategory', backref='item', lazy=True, nullable=True)
-    seller_id = db.Column(db.Integer, db.ForeignKey('seller.id'), nullable=True)
-    buyer_id = db.Column(db.Integer, db.ForeignKey('buyer.id'), nullable=True)
-    image = db.relationship('Image', backref='item', lazy=True, nullable=True)
+    category = db.relationship('Category', backref='item', lazy=True)
+    subcategory = db.relationship('Subcategory', backref='item', lazy=True)
+    image = db.relationship('Image', backref='item', lazy=True)
 
     def __repr__(self):
         return '<Item %r>' % self.name
@@ -47,7 +47,8 @@ class Category(db.Model):
 class Subcategory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    category = db.relationship('Subcategory', backref='item', lazy=True)
+    category = db.relationship('Category', backref='subcategory', lazy=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=True)
 
     def __repr__(self):
