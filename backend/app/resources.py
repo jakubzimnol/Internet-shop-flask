59390dc7e25f08c482734181e3f969d9dcd4202c
jakubzimnol <jakubzimnol@gmail.com>
2018-10-23 16:21:45 +0200

    add logging resources

--- backend/app/resources.py
+++ backend/app/resources.py
@@ -103,3 +103,44 @@ class SubcategoryList(Resource):
         subcategory = Subcategory(name=args['name'])
         add_to_database(subcategory)
         return marshal(subcategory, subcategory_marshaller), 201
+
+
+class UserRegistration(Resource):
+    def post(self):
+        return {'message': 'User registration'}
+
+
+class UserLogin(Resource):
+    def post(self):
+        return {'message': 'User login'}
+
+
+class UserLogoutAccess(Resource):
+    def post(self):
+        return {'message': 'User logout'}
+
+
+class UserLogoutRefresh(Resource):
+    def post(self):
+        return {'message': 'User logout'}
+
+
+class TokenRefresh(Resource):
+    def post(self):
+        return {'message': 'Token refresh'}
+
+
+class AllUsers(Resource):
+    def get(self):
+        return {'message': 'List of users'}
+
+    def delete(self):
+        return {'message': 'Delete all users'}
+
+
+class SecretResource(Resource):
+    def get(self):
+        return {
+            'answer': 42
+        }
+
--- backend/init_app.py
+++ backend/init_app.py
@@ -20,15 +20,22 @@ db = SQLAlchemy(application)
 migrate = Migrate(application, db)
 
 
-from app.resources import ItemsList, Items, CategoryList, Categories, SubcategoryList, Subcategories
+import app.resources as resources
 
 
-api.add_resource(ItemsList, '/api/item')
-api.add_resource(Items, '/api/item/<item_id>')
-api.add_resource(CategoryList, '/api/category')
-api.add_resource(Categories, '/api/category/<category_id>')
-api.add_resource(SubcategoryList, '/api/subcategory')
-api.add_resource(Subcategories, '/api/subcategory/<subcategory_id>')
+api.add_resource(resources.ItemsList, '/api/item')
+api.add_resource(resources.Items, '/api/item/<item_id>')
+api.add_resource(resources.CategoryList, '/api/category')
+api.add_resource(resources.Categories, '/api/category/<category_id>')
+api.add_resource(resources.SubcategoryList, '/api/subcategory')
+api.add_resource(resources.Subcategories, '/api/subcategory/<subcategory_id>')
+api.add_resource(resources.UserRegistration, '/api/registration')
+api.add_resource(resources.UserLogin, '/api/login')
+api.add_resource(resources.UserLogoutAccess, '/api/logout/access')
+api.add_resource(resources.UserLogoutRefresh, '/api/logout/refresh')
+api.add_resource(resources.TokenRefresh, '/api/token/refresh')
+api.add_resource(resources.AllUsers, '/api/users')
+api.add_resource(resources.SecretResource, '/api/secret')
 
 if __name__ == '__main__':
     application.run(debug=True)
