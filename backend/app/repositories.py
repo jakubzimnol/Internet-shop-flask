6614c084b56b3ebf59b335ab96872a118125f52f
jakubzimnol <jakubzimnol@gmail.com>
2018-10-25 14:08:08 +0200

    add generic creating object from dic function to repository

--- backend/app/repositories.py
+++ backend/app/repositories.py
@@ -1,11 +1,11 @@
 from sqlalchemy import exc
 
-from app.models import Image
 from init_app import db
 
 
-class Repository():
-    def add_to_database(self, item):
+class Repository:
+    @staticmethod
+    def add_to_database(item):
         try:
             db.session.add(item)
             db.session.commit()
@@ -14,8 +14,10 @@ class Repository():
             db.session.rollback()
             return False
 
-    def upload_image(self, image_dict):
-        new_image = Image(name=image_dict['name'], img_data=image_dict['img_data'], item_id=image_dict['item_id'])
-        db.session.add(new_image)
-        db.session.commit()
-
+    @classmethod
+    def create_and_add(cls, model, dictionary):
+        dict_without_none = {k: v for k, v in dictionary.items() if v is not None}
+        new_object = model(**dict_without_none)
+        if cls.add_to_database(new_object):
+            return new_object
+        return False
--- backend/app/resources.py
+++ backend/app/resources.py
@@ -35,10 +35,9 @@ class ItemsList(Resource):
 
     def post(self):
         args = creating_item_parser.parse_args()
-        item = Item(name=args['name'])
-        if not Repository.add_to_database(item):
+        item = Repository.create_and_add(Item, args)
+        if not item:
             return 404
-        item.update(args)
         db.session.commit()
         return marshal(item, item_marshaller), 201
 
@@ -67,8 +66,8 @@ class CategoryList(Resource):
 
     def post(self):
         args = creating_category_parser.parse_args()
-        category = Category(name=args['name'])
-        if not Repository.add_to_database(category):
+        category = Repository.create_and_add(Item, args)
+        if not category:
             return 404
         return marshal(category, category_marshaller), 201
 
@@ -97,7 +96,7 @@ class SubcategoryList(Resource):
 
     def post(self):
         args = creating_subcategory_parser.parse_args()
-        subcategory = Subcategory(name=args['name'])
-        if not Repository.add_to_database(subcategory):
+        subcategory = Repository.create_and_add(args)
+        if not subcategory:
             return 404
         return marshal(subcategory, subcategory_marshaller), 201
