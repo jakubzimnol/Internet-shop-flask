c36e414d654c6ae7114a26d131698c41abd8b1fe
jakubzimnol <jakubzimnol@gmail.com>
2018-10-29 14:56:49 +0100

    make more genreic object_exist() method -> enable adding name

--- backend/app/parsers.py
+++ backend/app/parsers.py
@@ -1,22 +1,22 @@
 from flask_restful import reqparse
-from sqlalchemy import exists
 
 from app.models import Category, Subcategory, Item, User
 from init_app import db
 
 
-def object_exist(model, value):
-    return db.session.query(exists().where(model.name == value)).scalar()
+def object_exist(model, value, name):
+    user_attribute = getattr(model, name)
+    return db.session.query(model.query.filter(user_attribute == value).exists()).scalar()
 
 
-def check_object(model, value):
-    if not object_exist(model, value):
+def check_object(model, value, name='name'):
+    if not object_exist(model, value, name):
         raise ValueError(f"{value} is not class instance")
     return value
 
 
-def check_unique_object(model, value):
-    if object_exist(model, value):
+def check_unique_object(model, value, name='name'):
+    if object_exist(model, value, name):
         raise ValueError(f"{value} already exist")
     return value
 
@@ -42,30 +42,28 @@ def check_unique_subcategory(value):
 
 
 def check_unique_user(value):
-    if db.session.query(exists().where(User.username == value)).scalar():
-        raise ValueError(f"{value} already exist")
-    return value
+    return check_unique_object(User, value, 'username')
 
 
 item_parser = reqparse.RequestParser(bundle_errors=True)
 item_parser.add_argument('name', type=check_unique_item, help="That name already exist")
 item_parser.add_argument('category', type=check_category)
-item_parser.add_argument('subcategory', type=check_subcategory, help="This field must be the Subcategory instance")
+item_parser.add_argument('subcategory', type=check_subcategory)
 
 creating_item_parser = item_parser.copy()
 creating_item_parser.replace_argument('name', type=check_unique_item,
                                       required=True, help="Name must be unique")
 
 category_parser = reqparse.RequestParser()
-category_parser.add_argument('name', type=check_unique_category, help="That category already exist")
+category_parser.add_argument('name', type=check_unique_category)
 
 creating_category_parser = category_parser.copy()
 creating_category_parser.replace_argument('name', type=check_unique_category,
                                           required=True, help="Name must be unique")
 
 subcategory_parser = reqparse.RequestParser(bundle_errors=True)
-subcategory_parser.add_argument('name', type=check_unique_subcategory, help="That subcategory already exist")
-subcategory_parser.add_argument('category', type=check_category, help="Name must be unique")
+subcategory_parser.add_argument('name', type=check_unique_subcategory)
+subcategory_parser.add_argument('category', type=check_category)
 
 creating_subcategory_parser = subcategory_parser.copy()
 creating_subcategory_parser.replace_argument('name', type=check_unique_subcategory,
